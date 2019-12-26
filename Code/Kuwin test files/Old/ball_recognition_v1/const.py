67780c2c448108ca30141b7a2c0c5f55bf45639b
Kuwin <kew.wyke@gmail.com>
2019-12-26 00:03:49 -0600

    Redo file structure

---
+++ Code/Kuwin test files/Old/ball_recognition_v1/const.py
@@ -0,0 +1,21 @@
+""" Kuwin Wyke
+Midwestern State University
+Start: 6 November 2019
+End: work in progress
+
+This module is designed to be used in the ball recognition program in the
+folder named "ball_recognition_v1". It only contains constants.
+"""
+
+STOP1 = "stop"
+STOP2 = "s"
+FULL_CONTROL = "full control"
+DICTIONARY = "dict"
+CALIBRATE = "calibrate"
+CALIBRATE_STOP = "calibrate stop"
+DETECT_ON = "detect"
+DETECT_OFF = "detect stop"
+DEBUG_ON = "debug on"
+DEBUG_OFF = "debug off"
+EXIT1 = "exit"
+EXIT2 = "e"
