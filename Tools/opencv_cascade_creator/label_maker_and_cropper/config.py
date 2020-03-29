74fe5cb550dc3e43b22cb471f044fd1fc84ed565
Kuwin <kew.wyke@gmail.com>
2020-03-28 21:50:40 -0500

    Major overhaul
    - Implement convenient and useful GUI features
    - Put entire program into one file
    - Add functionality to remove invalid file types

--- Tools/opencv_cascade_creator/label_maker_and_cropper/config.py
+++ Tools/opencv_cascade_creator/label_maker_and_cropper/config.py
@@ -9,16 +9,37 @@ import cv2
 
 class Conf:
     VERSION = 2.0
+    WINDOW_NAME = f"Image labeler version {VERSION}"
     LABELED_IMG_FOLDER = "labeled"
     ORIG_IMG_FOLDER = "orig_images"
     CROPPED_IMG_FOLDER = "cropped_images"
     DELETED_IMG_FOLDER = "images_to_delete"
+    INVALID_IMG_FOLDER = "invalid_images"
     SKIP_FOLDER = "skipped"
     ORIG_AFTER_LABEL = "originals_after_labeling"
     LABEL_FILE = LABELED_IMG_FOLDER + "/info.lst"
+    MAX_UNSAVED = 5
     CROP_SIZE = 50
     # Max width before image resizing
     MAX_IMG_WIDTH = 700
+
+
+class TK_Window:
+    HEIGHT1 = 700
+    WIDTH1 = 700
+    HEIGHT2 = 300
+    WIDTH2 = 350
+
+    CV_BUTTON_TEXT = "OpenCV Labeling"
+    TF_BUTTON_TEXT = "TensorFlow Labeling"
+
+    SAVE_CURRENT = "Press 'Space Bar' or 'Enter' to Save bounding boxes"
+    SAVE_ALL = "Press 's' to Force save info.lst file"
+    GO_BACK = "Press 'backspace' OR 'b' to go back"
+    RESTART = "Press 'r' to Restart labeling"
+    SKIP = "Press 'x' to Skip image"
+    DELETE = "Press 'd' to Delete current image"
+    MASK = "Press 'm' to Activate/Deactivate section masker"
 
 
 class CV_Window:
@@ -42,7 +63,7 @@ class CV_Window:
         "  'r' to restart labeling",  # Line 4
         "  'x' to skip image",  # Line 5
         "  'd' to delete current image",  # Line 6
-        "  'b' to activate/deactivate section blocker",  # Line 7
+        "  'm' to activate/deactivate section masker",  # Line 7
     ]
 
     X_ORG = 0
