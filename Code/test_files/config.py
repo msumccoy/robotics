73eccd5007dd0d808b02a4707f9ee7e96a744f54
kwyke <54901117+wykek@users.noreply.github.com>
2021-02-12 07:58:16 -0600

    Make fifo/pipe and start testing pipes

--- Code/test_files/config.py
+++ Code/test_files/config.py
@@ -5,7 +5,7 @@ import subprocess
 from variables import Alert
 
 config_file = "/home/pi/file.json"  # This is system specific
-# config_file = "/home/kuwin/file2.json"  # This is system specific
+config_file = "/home/kuwin/file2.json"  # This is system specific
 with open(config_file) as file:
     config = json.load(file)
 
@@ -13,11 +13,15 @@ with open(config_file) as file:
 class Conf:
     PATH_ROOT = config["path"]  # Typically: /home/pi/Documents/Code/
     PATH_ROOT += "test_files/"  # only for test environment
-    RAM_DISK_NAME = "system_ramdisk"
+    RAM_DISK = "system_ramdisk"
+    PIPE_PATH = f"{PATH_ROOT}{RAM_DISK}/pipes/"
+    PIPE_GUI = f"{PIPE_PATH}gui"  # FIFO for gui
     sub_proc = subprocess.run(["cat", "/proc/mounts"], stdout=subprocess.PIPE)
-    if RAM_DISK_NAME in sub_proc.stdout.decode("utf-8"):
+    if RAM_DISK in sub_proc.stdout.decode("utf-8"):
         Alert.ramdisk = True
-        PIPE_PATH = f"{PATH_ROOT}pipes/"
         if not os.path.exists(PIPE_PATH):
             os.mkdir(PIPE_PATH)
+
+        if not os.path.exists(PIPE_GUI):
+            os.mkfifo(PIPE_GUI)
 
--- Code/test_files/main.py
+++ Code/test_files/main.py
@@ -8,22 +8,42 @@ environment.
 """
 
 # TODO: Verify existence of ramdisk
-# TODO: create pipe
-#  communicate via pipes
 # TODO: create simple robot communication
 # TODO: create simple camera interface
 # TODO: create simple gui
-# TODO: get all three separate processes to communicate via pipes
+# TODO: Camera and robot in same thread gui in secondary process
+# TODO: get GUI processes to communicate via pipe
 
 
 from config import Conf
 from variables import Alert
 
 
+def mock_robot():
+    print("mock robot")
+    with open(Conf.PIPE_GUI, "w") as pipe:
+        # This needs to be done in a sub process as opening the pipe locks
+        # the system up until read from
+        print("writing to pipe")
+        pipe.write("test")
+        print("wrote to pipe")
+
+
+def mock_camera():
+    pass
+
+
+def mock_gui():
+    with open(Conf.PIPE_GUI, "r") as pipe:
+        pipe.read(4)
+
+
 def main():
     if Alert.ramdisk:
         # create and start processes
-        print("Start all processes")
+        print("Start gui process and remainder of program is in main process")
+        mock_robot()
+        mock_gui()
     else:
         print(
             "only start robot control (auto initially disabled)(cli control)"
