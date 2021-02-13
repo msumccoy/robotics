af621081ea1caf55ab5ce6764c13a916f320a91c
kwyke <54901117+wykek@users.noreply.github.com>
2021-02-12 20:38:05 -0600

    Start preparing for socket testing in conjunction to multi processing

--- Code/test_files/config.py
+++ Code/test_files/config.py
@@ -2,7 +2,6 @@ import json
 import os
 import subprocess
 
-from variables import Active
 
 config_file = "/home/pi/file.json"  # This is system specific
 config_file = "/home/kuwin/file2.json"  # This is system specific
@@ -13,8 +12,3 @@ with open(config_file) as file:
 class Conf:
     PATH_ROOT = config["path"]  # Typically: /home/pi/Documents/Code/
     PATH_ROOT += "test_files/"  # only for test environment
-    PIPE_PATH = f"{PATH_ROOT}/pipes/"
-    PIPE_GUI = f"{PIPE_PATH}gui"  # FIFO for gui
-    RAM_DISK = "system_ramdisk"
-    if not os.path.exists(PIPE_GUI):
-        os.mkfifo(PIPE_GUI)
--- Code/test_files/main.py
+++ Code/test_files/main.py
@@ -1,50 +1,14 @@
 """
-This file is to be used to test several concepts such as creation of ram
-disks, pipes, communication between separate processes, running tkinter in
-separate process from the rest of the program. If multiprocessing still
-produces error we will have to create two entirely different programs that
-communicate via pipes the same way they would communicate in a multiprocessing
-environment.
+Currently the objctive is to test passing video stream over sockets
 """
 
-# TODO: Verify existence of ramdisk
-# TODO: create simple robot communication
-# TODO: create simple camera interface
-# TODO: create simple gui
-# TODO: Camera and robot in same thread gui in secondary process
-# TODO: get GUI processes to communicate via pipe
-
-
-import subprocess
-import threading
-import time
+# TODO: test connecting two separate processes via sockets
 
 from config import Conf
-from variables import Active
-
-
-def pipe_handle():
-    with open(Conf.PIPE_GUI, "w", 0) as pipe:
-        # This needs to be done in a sub process as opening the pipe locks
-        # the system up until read from
-        print("writing to pipe")
-        pipe.write("test test")
-        print("test test")
-        time.sleep(1)
-        pipe.write("test1 test1")
-        print("test test2")
-        time.sleep(1)
-        pipe.write("test2 test2")
-        print("test test2]1")
-        time.sleep(1)
-        print("wrote to pipe")
 
 
 def mock_robot():
-    # Named pipe test: writing side can lock with no problem as it will be
-    # running it an independent thread
-    print("mock robot")
-    pipe_handle()
+    pass
 
 
 def mock_camera():
@@ -52,27 +16,11 @@ def mock_camera():
 
 
 def mock_gui():
-    # Named pipe test: Reading should not lock if writing side is not
-    # currently writing because this will result in GUI locking up
-    #
-    # On that note there needs to be at least two pipes:
-    #       one for the video, one for the text info
-    print("reading pipe")
-    with open(Conf.PIPE_GUI, "r", 0) as pipe:
-        print(pipe.read(50))
-    print("reading complete")
+    pass
 
 
 def main():
-    if Active.ramdisk:
-        # create and start processes
-        print("Start gui process and remainder of program is in main process")
-        mock_robot()
-        mock_gui()
-    else:
-        print(
-            "only start robot control (auto initially disabled)(cli control)"
-        )
+    pass
 
 
 if __name__ == '__main__':
