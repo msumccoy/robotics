7af0920e589811bd226c2dbc4c1e64db65192398
kuwin_wyke <54901117+wykek@users.noreply.github.com>
2021-10-10 16:59:21 -0500

    Start rework

---
+++ simulation/sim_objects.py
@@ -0,0 +1,6 @@
+"""
+This holds all coding simulation objects such as score num, robot, goal, ect.
+"""
+
+
+class ScoreNum(pygame)
--- simulation/variables.py
+++ simulation/variables.py
@@ -19,3 +19,11 @@ class DoFlag:
 
 class Gen:
     key_a_pressed = 0
+
+
+class Sprites:
+    from pygame.sprite import Group
+    every = Group()
+    robots = Group()
+    balls = Group()
+    goals = Group()
