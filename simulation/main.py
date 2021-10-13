11d705ffb895971ba0b55562781151dbdede64d6
kuwin_wyke <54901117+wykek@users.noreply.github.com>
2021-10-12 22:03:21 -0500

    Prepare to convert to multiprocessing

--- simulation/main.py
+++ simulation/main.py
@@ -2,11 +2,7 @@ import sys
 import pygame
 import random
 
-from config import Conf
-from sim_objects import Robot, Ball, Goal, ScoreNum
-
-from variables import ExitCtr, DoFlag, Sprites, Gen, Frames
-from controls import Controllers
+from sim_master import SimMaster
 
 
 def main():
@@ -18,44 +14,10 @@ def main():
         "Namely robot collision, and ball collision (for blocking balls "
         "kicked by other players)"
     )
-    pygame.init()
 
-    # Set up simulation window
-    Gen.screen = pygame.display.set_mode(Conf.WIN_SIZE)
-    background = pygame.Surface(Conf.WIN_SIZE).convert()
-    background.fill(Conf.WHITE)
-    Gen.screen.blit(background, (0, 0))
+    sim_masters = [SimMaster(0), SimMaster(1)]
 
-    # Create the robot, ball, and goals
-    robots = [Robot(side=Conf.LEFT)]
-    # robots = [Robot(side=Conf.RIGHT)]
-    balls = [Ball()]
-    goal_left = Goal(Conf.LEFT)
-    goal_right = Goal(Conf.RIGHT)
-
-    controller = Controllers(robots[0])
-
-    # Create clock for consistent loop intervals
-    clock = pygame.time.Clock()
-    while ExitCtr.gen:
-        Gen.screen.fill(Conf.WHITE)  # Reset screen for fresh drawings
-        # Control loop intervals
-        clock.tick(Conf.FPS)
-
-        # Control robot
-        controller.manual_control()
-        controller.check_score()
-        if DoFlag.auto_calc:
-            controller.calculated_control()
-
-        # Update game state
-        Sprites.every.update()
-        Sprites.every.draw(Gen.screen)
-        pygame.display.update()
-        Frames.update()
-
-    controller.save()
-    pygame.quit()
+    sim_masters[0].start()
 
 
 if __name__ == '__main__':
---
+++ simulation/sim_master.py
@@ -0,0 +1,48 @@
+import pygame
+
+from config import Conf
+from controls import Controllers
+from variables import Gen, ExitCtr, Frames, Sprites, DoFlag
+from sim_objects import Robot, Ball, Goal
+
+
+class SimMaster:
+    def __init__(self, index):
+        self.index = index
+
+    def start(self):
+        pygame.init()
+        # Set up simulation window
+        Gen.screen = pygame.display.set_mode(Conf.WIN_SIZE)
+        background = pygame.Surface(Conf.WIN_SIZE).convert()
+        background.fill(Conf.WHITE)
+        Gen.screen.blit(background, (0, 0))
+
+        # Create the robot, ball, and goals
+        self.robots = [Robot(side=Conf.LEFT)]
+        self.balls = [Ball()]
+        self.goals = [Goal(Conf.LEFT), Goal(Conf.RIGHT)]
+
+        self.controller = Controllers(self.robots[0])
+
+        # Create clock for consistent loop intervals
+        self.clock = pygame.time.Clock()
+        while ExitCtr.gen:
+            Gen.screen.fill(Conf.WHITE)  # Reset screen for fresh drawings
+            # Control loop intervals
+            self.clock.tick(Conf.FPS)
+
+            # Control robot
+            self.controller.manual_control()
+            self.controller.check_score()
+            if DoFlag.auto_calc:
+                self.controller.calculated_control()
+
+            # Update game state
+            Sprites.every.update()
+            Sprites.every.draw(Gen.screen)
+            pygame.display.update()
+            Frames.update()
+
+        self.controller.save()
+        pygame.quit()
