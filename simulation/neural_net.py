22d4b47cc8bd5055a10e921e89d0d8b7fc5863fc
kuwin_wyke <54901117+wykek@users.noreply.github.com>
2021-10-21 19:45:47 -0500

    Start writing frame_step tester

--- simulation/neural_net.py
+++ simulation/neural_net.py
@@ -1,5 +1,27 @@
-from sim_master import SimMaster
 
 
-mast = SimMaster(0)
-mast.frame_step()
+def frame_step_tester():
+    """
+    This function is used to test sim_master.SimMaster.frame_step
+    as well helps demonstrate how to utilize the method
+    """
+    import multiprocessing
+    from sim_master import SimMaster
+
+    from config import Conf
+
+    def neural_net_simulator():
+        pass
+
+    net_sims = []
+    for i in range(Conf.NUM_PROC):
+        net_sims.append(multiprocessing.Process(target=neural_net_simulator))
+
+    for proc in net_sims:
+        proc.start()
+    for proc in net_sims:
+        proc.join()
+
+
+if __name__ == "__main__":
+    frame_step_tester()
